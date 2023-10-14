ef97adc519f4b6612f4274488e2737ed8e4e685a
Sumit Saurabh <sumitsaurabh112@gmail.com>
2023-10-14 07:19:14 +0530

    Update Notes.jsx

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,53 +1,65 @@
-import { Box, Button, Card, CardFooter, CardHeader, Center, Heading, SimpleGrid} from '@chakra-ui/react';
+import { Box, Button, Card, CardFooter, CardHeader, Center, Heading, SimpleGrid, Text } from '@chakra-ui/react';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Notes(props) {
-    const navigate = useNavigate()
-    const [notes, setNotes] = useState([])
-    const getNotes = async()=>{
-       try {
-         const token = localStorage.getItem("myToken")
-        const {data} = await axios.get(`${process.env.REACT_APP_BASE_URL}/notes/get`,{
-            headers:{
-                Authorization: `Bearer ${token}`
-            }
-        })
-        setNotes(data.notes);
-       } catch (error) {
-        console.error(error)
-       }
+  const navigate = useNavigate();
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true); 
+  const getNotes = async () => {
+    try {
+      const token = localStorage.getItem("myToken");
+      const { data } = await axios.get(`${process.env.REACT_APP_BASE_URL}/notes/get`, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      setNotes(data.notes);
+      setLoading(false); 
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    useEffect(()=>{
-        getNotes()
-    },[notes])
-    return (
-       <Box w="100vw" h="100vh">
-  <SimpleGrid columns={{ sm: 2, md: 3, lg: 4 }} spacing={4}>
-    {notes &&
-      notes.map((note) => {
-        return (
-          <Box key={note._id} p="4" borderWidth="1px" borderRadius="md">
-            <Card >
-                <Center>
-        <CardHeader>
-          <Heading size="md">{note.title}</Heading>
-        </CardHeader>
-         </Center>
-         <Center>
-        <CardFooter>
-          <Button onClick={()=>navigate(`/notes/${note._id}`)}>View Notes</Button>
-        </CardFooter>
+  useEffect(() => {
+    getNotes();
+  }, [notes]);
+
+  return (
+    <Box w="100vw" h="100vh">
+      {loading ? ( 
+        <Center h="100%">
+          <Text>Loading...</Text>
         </Center>
-      </Card>
-          </Box>
-        );
-      })}
-  </SimpleGrid>
-</Box>
-    );
+      ) : notes.length === 0 ? ( 
+        <Center h="100%">
+          <Text>Create your first notes</Text>
+        </Center>
+      ) : (
+        <SimpleGrid columns={{ sm: 2, md: 3, lg: 4 }} spacing={4}>
+          {notes.map((note) => {
+            return (
+              <Box key={note._id} p="4" borderWidth="1px" borderRadius="md">
+                <Card>
+                  <Center>
+                    <CardHeader>
+                      <Heading size="md">{note.title}</Heading>
+                    </CardHeader>
+                  </Center>
+                  <Center>
+                    <CardFooter>
+                      <Button onClick={() => navigate(`/notes/${note._id}`)}>View Notes</Button>
+                    </CardFooter>
+                  </Center>
+                </Card>
+              </Box>
+            );
+          })}
+        </SimpleGrid>
+      )}
+    </Box>
+  );
 }
 
 export default Notes;
